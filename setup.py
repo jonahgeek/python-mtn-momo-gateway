3273ae4ecb88a7216126dab4a0cff04b5c9824b2
jonahgeek <jonahgeek@gmail.com>
2023-08-14 09:58:48 +0300

    add pyproject.toml

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='momogateway',
-    version='0.1.0',
-    description='MTN MOMO Gateway - a Python package that simplifies the process of interacting with the MTN Mobile Money (MoMo) API, making it easy to create API users, generate API keys, and obtain bearer tokens for secure transactions.',
+    version='0.1.1',
+    description='MTN MOMO Gateway - simplifies the process of interacting with the MTN Mobile Money API, making it easy to create API users, generate API keys.',
     author='Jonathan Mwebaze',
     author_email='jonahgeek@gmail.com',
     packages=find_packages(),
