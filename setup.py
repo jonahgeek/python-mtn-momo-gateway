a80ff0fee3c6432dbf8b6a53ee8c3de0ffa76eea
jonahgeek <jonahgeek@gmail.com>
2023-08-14 09:39:44 +0300

    add description to setup

--- setup.py
+++ setup.py
@@ -3,11 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name='momogateway',
     version='0.1.0',
-    description='MTN Momo Gateway',
+    description='MTN MOMO Gateway - a Python package that simplifies the process of interacting with the MTN Mobile Money (MoMo) API, making it easy to create API users, generate API keys, and obtain bearer tokens for secure transactions.',
     author='Jonathan Mwebaze',
     author_email='jonahgeek@gmail.com',
     packages=find_packages(),
     install_requires=[
         'requests',
     ],
+    url='https://github.com/jonahgeek/python-mtn-momo-gateway',
+    project_urls={
+        'Author': 'https://mwebaze.com',
+    }
 )
